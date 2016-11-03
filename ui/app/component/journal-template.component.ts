5923c29138bea2048da5ab61d0a0631eedd930cd
Karpuk Dmitry <karpuk_de_13@student.grsu.by>
2016-11-03 16:41:10 +0300

    fixed bugs

--- ui/app/component/journal-template.component.ts
+++ ui/app/component/journal-template.component.ts
@@ -15,29 +15,30 @@ export class JournalTemplateComponent {
     private template: JournalTemplateModel;
     private newTemplate: JournalTemplateModel;
     private templates: Array<JournalTemplateModel>;
+    private index: number;
+    private isSelected: boolean;
     private errorMessage: string;
 
     constructor(private journalTemplateService: JournalTemplateService){
         this.newTemplate = new JournalTemplateModel();
+        this.index = 0;
+        this.isSelected = true;
     }
 
-    private createTemplate(index: number): void{
+    private selectTemplate(index: number): void{
+        this.index = index;
+        this.isSelected = true;
+    }
+
+    private createTemplate(): void{
 
         if(this.template !== undefined) {
-
-            if(this.templates === undefined || this.templates == null) {
-                this.templates = [this.template];
-            }
-
-            let searchedTemplate = this.findTemplate(index, this.templates);
+            let searchedTemplate = this.findTemplate(this.index, this.templates);
             if (searchedTemplate != null) {
                 if (searchedTemplate.child === undefined || searchedTemplate.child == null) {
                     searchedTemplate.child = [];
                 }
-                if(this.newTemplate.child === undefined || this.newTemplate.child == null){
-                    this.newTemplate.child = [];
-                }
-                this.newTemplate.index = (searchedTemplate.index + 1) * 10 + this.newTemplate.child.length;
+                this.newTemplate.index = (searchedTemplate.index + 1) * 10 + searchedTemplate.child.length;
                 searchedTemplate.child.push(this.newTemplate);
                 this.newTemplate = new JournalTemplateModel();
             }
@@ -47,8 +48,12 @@ export class JournalTemplateComponent {
             this.newTemplate.child = [];
             this.template = this.newTemplate;
             this.newTemplate = new JournalTemplateModel();
-        }
 
+            if(this.templates === undefined || this.templates == null) {
+                this.templates = [this.template];
+            }
+        }
+        this.isSelected = false;
         console.log(this.template);
     }
 
