eabde35f5fe46df54f94a3b1460dde91a12e184a
Karpuk Dmitry <karpuk_de_13@student.grsu.by>
2016-11-09 11:04:28 +0300

    added h2 database

---
+++ web/src/main/java/com/smartjournal/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.smartjournal.config;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by karpukdm on 11/9/16.
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
