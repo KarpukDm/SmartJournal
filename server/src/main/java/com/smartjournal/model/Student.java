3aa406973ecb33a7e9376d4e222f0d1508b36eb6
KarpukDM <Wtpmjgda.1>
2016-12-24 15:26:08 +0300

    added lombok

--- server/src/main/java/com/smartjournal/model/Student.java
+++ server/src/main/java/com/smartjournal/model/Student.java
@@ -1,4 +1,16 @@
 package com.smartjournal.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Student {
+
+    @Id
+    private Long id;
 }
