61b07954b9ae41c204cddf1486a1ccbe0d6d777e
KarpukDM <Wtpmjgda.1>
2016-12-24 23:16:25 +0300

    fixed some bugs

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import {AppComponent} from "./component/app/app.component";
 import {TemplateCreatorComponent} from "./component/template-creator/template-creator.component";
 
 export const appRoutes: Routes = [
-  { path: '/', component: AppComponent },
+  { path: '', component: AppComponent },
   { path: 'template/create', component: TemplateCreatorComponent },
 ];
