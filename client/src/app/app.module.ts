cf49bd9ab5de06819bd40d86dd21d0d9befbc10d
KarpukDM <Wtpmjgda.1>
2016-12-24 22:39:54 +0300

    Added routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './component/app/app.component';
+import { TemplateCreatorComponent } from './component/template-creator/template-creator.component';
+import {RouterModule} from "@angular/router";
+import {appRoutes} from "./app.routes";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateCreatorComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
