7c1ffb3cb96866fb43ea33d8f1157ac44b1ed31a
KarpukDM <Wtpmjgda.1>
2016-12-26 19:44:56 +0300

    Update components

---
+++ client/src/app/component/template-builder/template-builder.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-builder',
+  templateUrl: './template-builder.component.html',
+  styleUrls: ['./template-builder.component.css']
+})
+export class TemplateBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+}
