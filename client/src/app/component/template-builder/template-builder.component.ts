30d09095dc04fad33f83eb2bf297a0ab79a4451d
KarpukDM <Wtpmjgda.1>
2016-12-28 23:54:42 +0300

    Added goUp()

--- client/src/app/component/template-builder/template-builder.component.ts
+++ client/src/app/component/template-builder/template-builder.component.ts
@@ -50,6 +50,12 @@ export class TemplateBuilderComponent implements OnInit {
     this.layerHistory.push(layer);
   }
 
+  private goUp(){
+    if(this.layerHistory.length > 0) {
+      this.layerHistory.pop();
+    }
+  }
+
   private getLayers() {
     if (!isNullOrUndefined(this.layerHistory)) {
       if(!isNullOrUndefined(this.layerHistory.length > 0 && !isNullOrUndefined(this.layerHistory.slice(-1)[0].layers))) {
