0ad6625d2d38a56c30fc101da302a6e3d7c8fa0b
KarpukDM <Wtpmjgda.1>
2017-03-21 23:20:26 +0300

    remove observer service

--- server/src/main/java/com/smartjournal/repository/ObserverRepository.java
+++
@@ -1,6 +0,0 @@
-package com.smartjournal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ObserverRepository extends JpaRepository<Observer,Long> {
-}
---
+++ server/src/main/java/com/smartjournal/service/impl/AccountServiceImpl.java
@@ -0,0 +1,14 @@
+package com.smartjournal.service.impl;
+
+import com.smartjournal.model.Account;
+import com.smartjournal.repository.AccountRepository;
+import com.smartjournal.service.common.impl.AbstractServiceImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountServiceImpl extends AbstractServiceImpl<Account, Long, AccountRepository> {
+
+    public AccountServiceImpl(AccountRepository repository) {
+        super(repository);
+    }
+}
