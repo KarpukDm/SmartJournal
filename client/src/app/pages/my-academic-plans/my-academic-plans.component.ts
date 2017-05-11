de8dbd72f87613ebe80947da793d1eebae32dfa7
karpukdm <offspace@yandex.ru>
2017-05-11 22:43:34 +0300

    Add myAcademicPlans component

---
+++ client/src/app/pages/my-academic-plans/my-academic-plans.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-academic-plans',
+  templateUrl: './my-academic-plans.component.html',
+  styleUrls: ['./my-academic-plans.component.css']
+})
+export class MyAcademicPlansComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
